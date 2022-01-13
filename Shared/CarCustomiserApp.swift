83b503398d71743402528c4d4bc83499eaddb0f1
James Tcheng <james.tcheng1234@gmail.com>
2022-01-13 21:08:40 +0000

    Initial Commit

---
+++ Shared/CarCustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarCustomiserApp.swift
+//  Shared
+//
+//  Created by Tcheng, James (BJH) on 13/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct CarCustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
