d233233bacdfa078cfeb2b0cf71ddc09a2050961
James Tcheng <james.tcheng1234@gmail.com>
2022-01-13 21:41:11 +0000

    First commit--car model and display stats method and their unit tests

---
+++ Shared/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  CarCustomiser
+//
+//  Created by Tcheng, James (BJH) on 13/01/2022.
+//
+
+import Foundation
+
+struct Car{
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+}
--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -9,34 +9,32 @@ import XCTest
 
 class Tests_iOS: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    func testNewCarGivesMeACarWithAllAttributesSet() {
+        let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+        XCTAssertEqual(car.make, "Mazda")
+        XCTAssertEqual(car.model, "MX-5")
+        XCTAssertEqual(car.topSpeed, 125)
+        XCTAssertEqual(car.acceleration, 7.7)
+        XCTAssertEqual(car.handling, 5)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    
+    func testDisplayStatsMethodGivesMeAllAttributesCorrectly(){
+        let car = Car(make: "Mazda", model: "MX-5", topSpeed: 125, acceleration: 7.7, handling: 5)
+        func displayStats(car: Car)->String{
+            var stats: String
+            stats = ""
+            stats += "Make: \(car.make)"
+            stats += "\n"
+            stats += "Model: \(car.model)"
+            stats += "\n"
+            stats += "Top Speed: \(car.topSpeed)mph"
+            stats += "\n"
+            stats += "Acceleration (0-60): \(car.acceleration)s"
+            stats += "\n"
+            stats += "Handling: \(car.handling)"
+            return stats
         }
+        let stats = displayStats(car: car)
+        XCTAssertEqual(stats, "Make: Mazda\nModel: MX-5\nTop Speed: 125mph\nAcceleration (0-60): 7.7s\nHandling: 5")
     }
 }
